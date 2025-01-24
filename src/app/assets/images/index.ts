d0d778fc6bd79a3d19cc1949d891c6993fce2302
Adil sheikh <sheikhadil044@gmail.com>
2025-01-24 14:05:20 +0500

    create hero-section

---
+++ src/app/assets/images/index.ts
@@ -0,0 +1,33 @@
+import OneImg from './one-img.jpg';    
+import TwoImg from './two-img.jpg';        
+import ThreeImg from './three-img.jpg';  
+import BikeEvolutionLog from './Bike-Evolution-Log.png';
+import facebook from './facebook.png';
+import instagram from './instagram.png';
+import youtube from './youtube.png';  
+import KopieVonOne from './Kopie-von-one.jpg';
+import KopieVonTwo from './Kopie-von-two.jpg';
+import BIkeEvolution from './BIkeEvolution.jpg'
+import foam from './foam.png'
+import SpaceVideo from './SpaceVideo.png'
+
+
+export {
+
+    OneImg,
+    TwoImg,
+    ThreeImg, 
+    BikeEvolutionLog,
+    facebook,
+    instagram,
+    youtube,
+    KopieVonOne,
+    KopieVonTwo, 
+    BIkeEvolution,
+    foam,
+    SpaceVideo,
+    
+
+
+
+}
